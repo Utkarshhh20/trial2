750ac1d3042eb09178eef6562382d93b0430f39e
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-06-10 23:45:02 +0400

    Update trial2.py

--- trial2.py
+++ trial2.py
@@ -21,6 +21,13 @@ import os
 import sys
 import subprocess
 from streamlit_option_menu import option_menu
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 # check if the library folder already exists, to avoid building everytime you load the pahe
 if not os.path.isdir("/tmp/ta-lib"):
 
@@ -438,6 +445,27 @@ title_alignment2="""
     """
 st.sidebar.markdown(title_alignment2, unsafe_allow_html=True)
 #dashboard = st.sidebar.selectbox('', ('Home', 'Screener', 'Fundamental Analysis', 'Technical Indicators', 'Backtesting', 'Pattern Stocks'), 0)
+menu_data = [
+    {'icon': "far fa-copy", 'label':"Left End"},
+    {'id':'Copy','icon':"🐙",'label':"Copy"},
+    {'icon': "fa-solid fa-radar",'label':"Dropdown1", 'submenu':[{'id':' subid11','icon': "fa fa-paperclip", 'label':"Sub-item 1"},{'id':'subid12','icon': "💀", 'label':"Sub-item 2"},{'id':'subid13','icon': "fa fa-database", 'label':"Sub-item 3"}]},
+    {'icon': "far fa-chart-bar", 'label':"Chart"},#no tooltip message
+    {'id':' Crazy return value 💀','icon': "💀", 'label':"Calendar"},
+    {'icon': "fas fa-tachometer-alt", 'label':"Dashboard",'ttip':"I'm the Dashboard tooltip!"}, #can add a tooltip message
+    {'icon': "far fa-copy", 'label':"Right End"},
+    {'icon': "fa-solid fa-radar",'label':"Dropdown2", 'submenu':[{'label':"Sub-item 1", 'icon': "fa fa-meh"},{'label':"Sub-item 2"},{'icon':'🙉','label':"Sub-item 3",}]},
+]
+
+over_theme = {'txc_inactive': '#FFFFFF'}
+menu_id = hc.nav_bar(
+    menu_definition=menu_data,
+    override_theme=over_theme,
+    home_name='Home',
+    login_name='Logout',
+    hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
+    sticky_nav=True, #at the top or not
+    sticky_mode='sticky', #jumpy or not-jumpy, but sticky or pinned
+)
 dashboard = option_menu (
         menu_title=None, 
         options=['Home', 'Fundamental Analysis', 'Technical Indicators', 'Backtesting'], 
